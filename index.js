c46e744a0ed9b2f284b18d4ed7b5ba43ae5e922f
Amine <algusamine@gmail.com>
2021-06-29 17:25:09 -0400

    commit tree 04de045dbfcfe7f4bed2766d6f0ca2b74bef975d

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+};
+
+function setBestCustomer(){
+    bestCustomer ='not bob';
+    return bestCustomer;
+};
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+};
+const leastFavoriteCustomer = 'Mike'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer ='Asignment to constant variable.';
+}
